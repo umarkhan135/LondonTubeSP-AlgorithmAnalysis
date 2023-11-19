065c33cb70cc9eefd53485a09ddf09bc1955f683
Omar El Aref <o.k.elaref@gmail.com>
2023-11-18 19:03:46 -0500

    approximations done for part 1

---
+++ Part 1/bellmanford_relaxation.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.insert(0, '/Users/okela/OneDrive/Desktop/Repos/3XB3-FinalProject')
+from final_project_part1 import *
+from min_heap import *
+
+
+def bellman_ford_approx(G, source, k):
+    pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
+    dist = {} #Distance dictionary
+    relax = {}
+    nodes = list(G.adj.keys())
+    
+    for node in nodes:
+        relax[node] = 0
+
+    #Initialize distances
+    for node in nodes:
+        dist[node] = float("inf")
+    dist[source] = 0
+
+    #Meat of the algorithm
+    for _ in range(G.number_of_nodes()):
+        for node in nodes:
+            for neighbour in G.adj[node]:
+                if relax[neighbour] < k and dist[neighbour] > dist[node] + G.w(node, neighbour):
+                    dist[neighbour] = dist[node] + G.w(node, neighbour)
+                    pred[neighbour] = node
+                    relax[neighbour] += 1
+    return dist
+
+'''
+*************
+Testing
+*************
+'''
+
+g = DirectedWeightedGraph()
+for i in range(9):
+    g.add_node(i)
+g.add_edge(0, 1, 2)
+g.add_edge(0, 2, 1)
+g.add_edge(1, 3, 3)
+g.add_edge(1, 4, 5)
+g.add_edge(1, 5, 1)
+g.add_edge(2, 6, 7)
+g.add_edge(5, 7, 2)
+g.add_edge(6, 8, 3)
+g.add_edge(5, 4, 1)
+
+
+g1 = DirectedWeightedGraph()
+for i in range(6):
+    g1.add_node(i)
+    
+g1.add_edge(0, 1, 8)
+g1.add_edge(0, 2, 10)
+g1.add_edge(1, 3, 1)
+g1.add_edge(2, 5, 2)
+g1.add_edge(3, 2, -4)
+g1.add_edge(3, 5, -1)
+g1.add_edge(4, 2, 1)
+g1.add_edge(5, 4, -2)
+
+print(bellman_ford_approx(g1, 0, 1))
---
+++ Part 1/dijkstra_relaxation.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.insert(0, '/Users/okela/OneDrive/Desktop/Repos/3XB3-FinalProject')
+from final_project_part1 import *
+from min_heap import *
+
+
+
+def dijkstra_approx(G, source, k):
+    pred = {} #Predecessor dictionary. Isn't returned, but here for your understanding
+    dist = {} #Distance dictionary
+    relax = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+    
+    for node in nodes:
+        relax[node] = 0
+
+    #Initialize priority queue/heap and distances
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+    Q.decrease_key(source, 0)
+
+    #Meat of the algorithm
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+        for neighbour in G.adj[current_node]:
+            if relax[neighbour] < k and dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
+                relax[neighbour] += 1;
+    return dist
+
+'''
+*************
+Testing
+*************
+'''
+
+g = DirectedWeightedGraph()
+for i in range(9):
+    g.add_node(i)
+g.add_edge(0, 1, 2)
+g.add_edge(0, 2, 1)
+g.add_edge(1, 3, 3)
+g.add_edge(1, 4, 5)
+g.add_edge(1, 5, 1)
+g.add_edge(2, 6, 7)
+g.add_edge(5, 7, 2)
+g.add_edge(6, 8, 3)
+g.add_edge(5, 4, 1)
+
+#print(dijkstra_approx(g, 0, 1))
