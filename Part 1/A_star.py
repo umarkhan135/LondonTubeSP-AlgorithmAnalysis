24fff9d3676a5d63c25b1f2aeaf7df24f211c1cb
Omar El Aref <o.k.elaref@gmail.com>
2023-12-01 15:56:02 -0500

    part1 & 2 done with fix ups

---
+++ Part 1/A_star.py
@@ -0,0 +1,45 @@
+import min_heap
+from final_project_part1 import *
+
+
+def a_star(G, s, d, h):
+    pred = {}
+    dist = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+    
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+        pred[node] = None  # Initialize predecessors
+    Q.decrease_key(s, h[s])
+    dist[s] = 0
+    
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+
+        if current_node == d:
+            break
+
+        for neighbour in G.adj[current_node]:
+            if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
+                dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
+                pred[neighbour] = current_node
+                fn = dist[current_node] + G.w(current_node, neighbour) + h[neighbour]
+                Q.decrease_key(neighbour, fn)
+
+        
+    
+    
+    path = []
+    if pred[d] is not None or d == s:  # Check if d is reachable
+        while d is not None:
+            path.insert(0, d)
+            d = pred[d]
+
+    return pred, path
+
+
+
--- Part 1/Experiment_Suite_1.py
+++ Part 1/Experiment_Suite_1.py
@@ -9,7 +9,7 @@ import timeit
 *************
 Experiment 1
 *************
-'''
+
 
 
 def experiment_helper(G, k_value, source, num_trials):
@@ -52,7 +52,7 @@ num_trials = 10
 upper = 1000
 
 #experiment1(G_sizes, k_ratios, source, num_trials, upper)
-
+'''
 
 '''
 *************
@@ -61,7 +61,7 @@ Experiment 2
 '''
 
 
-def experiment2_helper(G, source, k_ratios, numtrials):
+def experiment1_helper(G, source, k_ratios, numtrials):
     results = {}
     graph_size = G.number_of_nodes()
     dist_standard = dijkstra(G, source)
@@ -80,11 +80,11 @@ def experiment2_helper(G, source, k_ratios, numtrials):
     
     return results
 
-def experiment2(G_sizes, k_ratios, source, upper, numtrials):
+def experiment1(G_sizes, k_ratios, source, upper, numtrials):
     results = {ratio: [] for ratio in k_ratios}
     for size in G_sizes:
         G = create_random_complete_graph(size, upper)
-        accuracies = experiment2_helper(G, source, k_ratios, numtrials)
+        accuracies = experiment1_helper(G, source, k_ratios, numtrials)
         for ratio in k_ratios:
             results[ratio].append(accuracies[ratio])
             
@@ -106,7 +106,7 @@ source2 = 0
 num_trials2 = 25
 upper2 = 1000
 
-#experiment2(G_sizes2, k_ratios2, source2, upper2, num_trials2)
+#experiment1(G_sizes2, k_ratios2, source2, upper2, num_trials2)
 
 
 '''
@@ -128,47 +128,51 @@ def create_sparse_graph(num_nodes, edge_probability, upper):
 
     return G
 
-def experiment3_helper(G, k_value, source, num_trials):
-    count = 0
-    for _ in range(num_trials):
-        start_time = timeit.default_timer()
-        dijkstra_approx(G, source, k_value)
-        end_time = timeit.default_timer()
-        execution_time = end_time - start_time
-        count += execution_time
-    return count/num_trials
+def experiment2_helper(G, source, k_ratios, numtrials):
+    results = {}
+    dist_standard = dijkstra(G, source)
+    total_distance_standard = total_dist(dist_standard)
 
+    for ratio in k_ratios:
+        relative_error = 0
+        for i in range(numtrials):
+            k = ratio
+            dist_approx = dijkstra_approx(G, source, k)
+            total_distance_approx = total_dist(dist_approx)
 
-def experiment3(G_sizes, k_ratios, source, num_trials, upper, probability):
+            # Correctly calculate accuracy
+            relative_error += abs(total_distance_approx - total_distance_standard) / total_distance_standard
+        results[ratio] = relative_error/numtrials
+    
+    return results
+
+def experiment2(G_size, k_ratios, source, upper, numtrials, probabilities):
     results = {ratio: [] for ratio in k_ratios}
     
-    for size in G_sizes:
-        g = create_sparse_graph(size, probability, upper)
+    for prob in probabilities:
+        G = create_sparse_graph(G_size, prob, upper)
+        accuracies = experiment2_helper(G, source, k_ratios, numtrials)
         for ratio in k_ratios:
-            k_value = int(size * ratio)
-            avg_time = experiment3_helper(g, k_value, source, num_trials)
-            results[ratio].append(avg_time)
-    
+            results[ratio].append(accuracies[ratio])
+            
     for ratio in k_ratios:
-        plt.plot(G_sizes, results[ratio], label=f'k_ratio = {ratio}')
-    
-    plt.xlabel('Graph sizes')
-    plt.ylabel('Time')
-    plt.title('Graph size VS time for certain k values')
-    plt.grid(True)
+        plt.plot(probabilities, results[ratio], label=f'k_ratio = {ratio}')
+    plt.title('Accuracy vs Graph Density for Different k Ratios')
+    plt.xlabel('Graph Density (Probability)')
+    plt.ylabel('Accuracy')
     plt.legend()
+    plt.grid(True)
     plt.show()
     
-G_sizes3 = []
-for i in range(100, 525, 25):
-    G_sizes3.append(i)
-k_ratios3 = [1, 1/2, 1/4, 1/25, 1/50]
+
+G_sizes3 = 200
+k_ratios3 = [1, 2, 5, 10, 15]
 source3 = 0
 num_trials3 = 10
 upper3 = 1000
-probability = 0.1
+probability = [0.1, 0.2, 0.3, 0.5, 0.75]
 
-#experiment3(G_sizes3, k_ratios3, source3, num_trials3, upper3, probability)
+#experiment2(G_sizes3, k_ratios3, source3, num_trials3, upper3, probability)
 
 
 
@@ -179,7 +183,7 @@ Experiment 4
 '''
 
 
-def experiment4_helper(G, source, k_ratios, numtrials):
+def experiment3_helper(G, source, k_ratios, numtrials):
     results = {}
     graph_size = G.number_of_nodes()
     dist_standard = dijkstra(G, source)
@@ -198,17 +202,17 @@ def experiment4_helper(G, source, k_ratios, numtrials):
     
     return results
 
-def experiment4(G_sizes, k_ratios, source, upper, numtrials, probability):
+def experiment3(G_sizes, k_ratios, source, upper, numtrials, probability):
     results = {ratio: [] for ratio in k_ratios}
     for size in G_sizes:
         G = create_sparse_graph(size, probability, upper)
-        accuracies = experiment4_helper(G, source, k_ratios, numtrials)
+        accuracies = experiment3_helper(G, source, k_ratios, numtrials)
         for ratio in k_ratios:
             results[ratio].append(accuracies[ratio])
             
     for ratio in k_ratios:
-        plt.plot(G_sizes, results[ratio], label=f'k_ratio = {ratio}')
-    plt.title('Accuracy vs Graph Size for Different k Ratios')
+        plt.plot(G_sizes, results[ratio], label=f'k_values = {ratio}')
+    plt.title('Accuracy vs Sparce Graphs for Different k Ratios')
     plt.xlabel('Graph Size')
     plt.ylabel('Accuracy')
     plt.legend()
@@ -225,6 +229,6 @@ num_trials4 = 25
 upper4 = 1000
 probability2 = 0.1
 
-#experiment4(G_sizes4, k_ratios4, source4, upper4, num_trials4, probability2)
+experiment3(G_sizes4, k_ratios4, source4, upper4, num_trials4, probability2)
 
 
