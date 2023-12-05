966ac860066888aad439168a49e85977323832dc
Umar Khan <1khanumar@gmail.com>
2023-12-05 02:42:08 -0500

    part3 tubeGraph class added and csv data is extracted. need to work on integrating dijkstra and A* with this and code experiments

--- part3.py
+++ part3.py
@@ -1,20 +1,95 @@
 import pandas as pd
 import numpy as np
+import time
+import itertools
+import matplotlib.pyplot as plt
+from final_project_part1 import *
 from A_star import *
-
-data = pd.read_csv("london_stations.csv")
-
-longitude = {}
-latitude = {}
-
-id_array = np.array(data["id"])
+from math import *
 
 
-latitude_array = np.array(data["latitude"])
-longitude_array = np.array(data["longitude"])
+def haversine(lon1, lat1, lon2, lat2):
+    # Convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-for i in range(len(id_array)):
-    longitude[id_array[i]] = longitude_array[i]
-    latitude[id_array[i]] = latitude_array[i]
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of Earth in kilometers. Use 3956 for miles
+    return c * r
+
+class TubeGraph:
+    def __init__(self):
+        self.edges = {}  # Edges will contain tuples of (node, weight)
+        self.coordinates = {}  # To store the latitude and longitude
+        self.destination = None  # To store the destination for the heuristic
+
+    def add_station(self, station_id, lat, lon):
+        self.coordinates[station_id] = (lat, lon)
+
+    def add_connection(self, station1, station2):
+        lon1, lat1 = self.coordinates[station1]
+        lon2, lat2 = self.coordinates[station2]
+        weight = haversine(lon1, lat1, lon2, lat2)
+        self.edges.setdefault(station1, []).append((station2, weight))
+        self.edges.setdefault(station2, []).append((station1, weight))
+
+    def set_destination(self, destination_id):
+        self.destination = destination_id
+
+    def initialize_adjacency(self):
+        self.adj = {station: set() for station in self.coordinates}
+        for station, connections in self.edges.items():
+            for (neighbour, _) in connections:
+                self.adj[station].add(neighbour)
+
+    def heuristic(self, node):
+        if self.destination is None or node not in self.coordinates or self.destination not in self.coordinates:
+            return float('inf')  # Return infinity if there is no destination or invalid nodes
+        
+        lon1, lat1 = self.coordinates[node]
+        lon2, lat2 = self.coordinates[self.destination]
+        return haversine(lon1, lat1, lon2, lat2)
+
+    def w(self, node1, node2):
+        for neighbour, weight in self.edges[node1]:
+            if neighbour == node2:
+                return weight
+        return float('inf')  # Return infinity if there is no direct connection
+
+stations_df = pd.read_csv('london_stations.csv')
+connections_df = pd.read_csv('london_connections.csv')
+
+tube_graph = TubeGraph()
+
+for index, row in stations_df.iterrows():
+    tube_graph.add_station(station_id=row['id'], lat=row['latitude'], lon=row['longitude'])
+
+for index, row in connections_df.iterrows():
+    tube_graph.add_connection(station1=row['station1'], station2=row['station2'])
+
+# destination_id = 2
+# tube_graph.set_destination(destination_id)
+
+# station_ids_to_check = [1, 2, 3, 4, 5]  # Example IDs, replace with actual station IDs you're interested in
+
+# for station_id in station_ids_to_check:
+#     heuristic_value = tube_graph.heuristic(station_id)
+#     print(f"Heuristic value for station {station_id} to destination {destination_id}: {heuristic_value}")
+    
+# for station_id in tube_graph.coordinates:
+#     lat, lon = tube_graph.coordinates[station_id]
+#     print(f"Station ID: {station_id}, Latitude: {lat}, Longitude: {lon}")
+
+#     if station_id in tube_graph.edges:
+#         print("Connections:")
+#         for connection in tube_graph.edges[station_id]:
+#             connected_station_id, weight = connection
+#             print(f"  Connected to Station ID: {connected_station_id} with weight (distance): {weight}")
+#     else:
+#         print("No connections")
+#     print()  
 
 
